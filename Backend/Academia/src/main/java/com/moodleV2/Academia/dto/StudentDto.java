fa68c28f8bd47ef500730207522e6b8fba31477e
Andrei <andrei.zacordonet@student.tuiasi.ro>
2024-12-05 00:02:46 +0200

    Fix: Use NotNull and add student id field.

--- Backend/Academia/src/main/java/com/moodleV2/Academia/dto/StudentDto.java
+++ Backend/Academia/src/main/java/com/moodleV2/Academia/dto/StudentDto.java
@@ -3,67 +3,58 @@ package com.moodleV2.Academia.dto;
 import com.moodleV2.Academia.models.Ciclu;
 import com.moodleV2.Academia.models.Student;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Size;
-import lombok.EqualsAndHashCode;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
-import lombok.NonNull;
+import lombok.Setter;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Schema(description = "DTO for creating or updating Student")
 @Getter
+@Setter
 public class StudentDto {
 
+    @Schema(description = "Id Student", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
+    @NotNull
+    private Long id;
+
     @Schema(description = "Nume Student", example = "George", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NonNull
+    @NotNull
     @Size(min = 1, max = 50)
     private String nume;
 
     @Schema(description = "Prenume Student", example = "Calinacio", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NonNull
+    @NotNull
     @Size(min = 1, max = 50)
     private String prenume;
 
     @Schema(description = "Email Student", example = "andrei74c0@gmail.com", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NonNull
+    @NotNull
     @Email
     private String email;
 
     @Schema(description = "Ciclul de studii", example = "LICENTA", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NonNull
+    @NotNull
     private Ciclu cicluStudii;
 
     @Schema(description = "An de studiu", example = "2", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NonNull
+    @NotNull
     @Min(1)
     @Max(5)
     private int anStudiu;
 
     @Schema(description = "Grupa", example = "9", requiredMode = Schema.RequiredMode.REQUIRED)
-    @NonNull
+    @NotNull
     @Min(1)
     @Max(50)
     private int grupa;
 
-    // DONE: implement discipline DTO
-    @Schema(description = "Classes inrolled by the student")
+    @Schema(description = "Classes enrolled by the student")
     private Set<DisciplinaDto> classes;
 
-    public StudentDto(String nume, String prenume, String email, Ciclu cicluStudii, int anStudiu, int grupa, Set<DisciplinaDto> classes) {
-        this.nume = nume;
-        this.prenume = prenume;
-        this.email = email;
-        this.cicluStudii = cicluStudii;
-        this.anStudiu = anStudiu;
-        this.grupa = grupa;
-        this.classes = classes;
-    }
-
     public StudentDto(Student student) {
+        this.id = student.getId();
         this.nume = student.getNume();
         this.prenume = student.getPrenume();
         this.email = student.getEmail();
@@ -93,6 +84,7 @@ public class StudentDto {
         if (this.classes != null) {
             student.getClasses().addAll(this.classes.stream().map(DisciplinaDto::DisciplinaMapper).collect(Collectors.toSet()));
         }
+
         return student;
     }
 }
