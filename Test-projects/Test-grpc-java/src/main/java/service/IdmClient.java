43be086fcd3fbc67dbba05f95cee1ecbb2fe13b8
Andrei <andrei.zacordonet@student.tuiasi.ro>
2024-12-18 14:21:52 +0200

    Test: Idm client complete.

--- Test-projects/Test-grpc-java/src/main/java/service/IdmClient.java
+++ Test-projects/Test-grpc-java/src/main/java/service/IdmClient.java
@@ -2,58 +2,77 @@ package service;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import proto.CalculatorGrpc;
-import proto.CalculatorOuterClass;
 import proto.IdmGrpc;
 import proto.IdmOuterClass;
 
+import java.util.Scanner;
+
 public class IdmClient {
     public static void main(String[] args) {
-        /* *
-         * Establish a connection to the server using the class ManagedChannelBuilder and the function usePlaintext().
-         * The function usePlainText() should only be used for testing or for APIs where the use of such API or the data
-         * exchanged is not sensitive.
-         * */
+        // Create a connection to the gRPC server
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
-
-        /* *
-         * A blocking-style stub instance of Greeter service. We can have two types of stubs: blocking and async.
-         * Blocking stubs are synchronous. Non-blocking stubs are asynchronous.
-         * Take care if you want to call an RPC function on a blocking stub from UI thread
-         * (cause an unresponsive/laggy UI).
-         * */
         IdmGrpc.IdmBlockingStub stub = IdmGrpc.newBlockingStub(channel);
 
-        /* *
-         * An asynchronous instance of the above declaration.
-         * IdmGrpc.IdmStub stub = IdmGrpc.newStub(channel);
-         * */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Connected to gRPC server on localhost:50051.");
 
-        /* *
-         * We can now use the gRPC function via our instance of GreeterBlockingStub bookStub.
-         * Below we call the function sayHello(Helloworld.HelloRequest request) with name field value set to "gRPC".
-         * This function returns a value of type  Helloworld.HelloReply that is saved in our instance reply.
-         * We can get via generated functions every field from our message, in this example, we have just one field.
-         * */
+        while (true) {
+            System.out.println("\nEnter command: login / validate / destroy / exit :");
+            String command = scanner.nextLine().trim().toLowerCase();
 
-        IdmOuterClass.AuthResponse authResponse = stub.authenticate(IdmOuterClass.AuthRequest.newBuilder().setEmail("andrei74c0@gmail.com").setPassword("pass1234").build());
+            if ("exit".equals(command)) {
+                System.out.println("Exiting client...");
+                break;
+            }
 
-        System.out.println(authResponse.getToken());
+            try {
+                switch (command) {
+                    case "login" -> {
+                        System.out.println("\nEnter credentials for authentication");
+                        System.out.print("Email: ");
+                        String email = scanner.nextLine().trim();
 
-        // TODO: catch io.grpc.StatusRuntimeException
-        IdmOuterClass.ValidateResponse validateResponse = stub.validate(IdmOuterClass.ValidateOrDestroyRequest.newBuilder().setToken(authResponse.getToken()).build());
+                        System.out.print("Password: ");
+                        String password = scanner.nextLine().trim();
 
-        System.out.println("Sub: " + validateResponse.getSub() + "\tRole: " + validateResponse.getRole());
+                        IdmOuterClass.AuthRequest authRequest = IdmOuterClass.AuthRequest.newBuilder()
+                                .setEmail(email)
+                                .setPassword(password)
+                                .build();
+                        IdmOuterClass.AuthResponse authResponse = stub.authenticate(authRequest);
+                        System.out.println("Received token: " + authResponse.getToken());
+                    }
 
-//        validateResponse = stub.validate(IdmOuterClass.ValidateOrDestroyRequest.newBuilder().setToken(authResponse.getToken() + "1").build());
-//
-//        System.out.println("Sub: " + validateResponse.getSub() + "\tRole: " + validateResponse.getRole());
+                    case "validate" -> {
+                        System.out.println("\nEnter JWT:");
+                        String jwt = scanner.nextLine().trim();
 
-        // TODO: catch io.grpc.StatusRuntimeException
-        IdmOuterClass.DestroyResponse destroyResponse = stub.destroy(IdmOuterClass.ValidateOrDestroyRequest.newBuilder().setToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvYXBpL2FjYWRlbWlhIiwic3ViIjoiMSIsImV4cCI6MTczNDQ0Mjc0NiwianRpIjoiYjAxM2RhMzktOTJlYi00MDJmLTg3Y2YtNTljNGIwMTNkODk0Iiwicm9sZSI6IkFETUlOIn0.T3nERPnNnrcQA53aXOqr--vGZr1mHLVeTREO5x71pjU").build());
+                        IdmOuterClass.ValidateOrDestroyRequest validateRequest = IdmOuterClass.ValidateOrDestroyRequest.newBuilder()
+                                .setToken(jwt)
+                                .build();
+                        IdmOuterClass.ValidateResponse validateResponse = stub.validate(validateRequest);
+                        System.out.println("Response:\nSub: " + validateResponse.getSub() + "\tRole: " + validateResponse.getRole());
+                    }
 
-        System.out.println(destroyResponse.getSuccess());
+                    case "destroy" -> {
+                        System.out.println("\nEnter JWT:");
+                        String jwt = scanner.nextLine().trim();
 
+                        IdmOuterClass.ValidateOrDestroyRequest destroyRequest = IdmOuterClass.ValidateOrDestroyRequest.newBuilder()
+                                .setToken(jwt)
+                                .build();
+                        IdmOuterClass.DestroyResponse destroyResponse = stub.destroy(destroyRequest);
+                        System.out.println("Response: " + destroyResponse.getSuccess());
+                    }
+
+                    default -> System.out.println("Invalid command. Please try again.");
+                }
+            } catch (io.grpc.StatusRuntimeException e) {
+                System.err.println("Error: " + e.getStatus().getCode() + " - " + e.getStatus().getDescription());
+            }
+        }
+
+        // Shutdown the channel
         channel.shutdown();
     }
 }
